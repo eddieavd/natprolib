ca62dc27c8849d83f80a02b28ba96d26b1f74866
Edhem Avdagic <edhemavdagic@gmail.com>
2021-02-11 16:29:52 +0100

    Initial Commit

---
+++ natprolib/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  natprolib
+//
+//  Created by Edhem Avdagic on 11/02/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
